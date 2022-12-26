f0394938f720f8aaccfccab855cc737c6163e99d
bit-dream <61420422+bit-dream@users.noreply.github.com>
2022-12-26 12:16:49 -0600

    Added better resize handling

--- components/buttons/LargeInsetButton.tsx
+++ components/buttons/LargeInsetButton.tsx
@@ -3,6 +3,8 @@ import Icon from '../Icon/Icon'
 
 const LargeInsetButton = (props: {text: string, heading: string}) => {
     const classList = twMerge(
+        'ml-1',
+        'mr-1',
         'bg-transparent',
         'border border-gray-600',
         'rounded-lg',
@@ -13,17 +15,18 @@ const LargeInsetButton = (props: {text: string, heading: string}) => {
         'hover:shadow-lg',
         'hover:shadow-sky-500/75',
         'hover:bg-white',
+        'dark:hover:bg-gray-800/60',
         'dark:bg-gray-800/30',
         'lg:p-5'
     );
     return <>
         <a className={classList}>
             <div className='flex flex-row h-full'>
-                <div className="flex flex-col h-full">
-                    <div className='text-sky-300 text-lg lg:text-2xl basis-1/2'>
+                <div className="flex flex-col self-center">
+                    <div className='text-sky-300 text-lg lg:text-2xl lg:basis-1/2 md:basis-1/2 h-full align-baseline'>
                         {props.heading}
                     </div>
-                    <div className='text-xs basis-1/2 dark:text-white'>{props.text}</div>
+                    <div className='text-xs basis-1/2 dark:text-white lg:block md:block hidden'>{props.text}</div>
                 </div>
                 <div className='self-center dark:text-white'><Icon type='arrow_forward'/></div>
             </div>
